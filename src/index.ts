7346841d128c0ad64e811ca0280f802c924ad64e
Faid Terence JABO <faidterence@outlook.com>
2023-08-13 14:05:10 +0200

    feature(User):SchemaDefined

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server =http.createServer(app);
+
+server.listen(8080, ()=> {
+    console.log(`Server started on http://localhost:8080!`);
+});
+
+const MONGO_URL = "mongodb+srv://terencefaid:thestunna420@cluster0.bdhur1p.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL)
+.then(()=> console.log("Database Connected.."));
+mongoose.connection.on(`error`, (error: Error)=> console.log(error));
