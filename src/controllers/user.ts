e81eab17d2c5acc3c30541636433b2fcd51634e9
Faid Terence JABO <faidterence@outlook.com>
2023-08-13 21:59:45 +0200

    feature(user):Update profile controllers

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers, deleteUserById } from "../db/Users";
+import { getUsers, deleteUserById, getUserById } from "../db/Users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -23,5 +23,25 @@ export const deleteUser = async (
     return res.status(202).json({ message: "User deleted Successfully" });
   } catch (error) {
     return res.status(409).json({ message: "Opps User not deleted try again" });
+  }
+};
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { username, age, gender } = req.body;
+    if (!username || !age || !gender) {
+      return res.status(400).json({ message: "Fill in the fields" });
+    }
+    const user = await getUserById(id);
+    user.username = username;
+    user.age = age;
+    user.gender = gender;
+    await user.save();
+    return res.status(202).json({ message: "Updated Successfully" });
+  } catch (error) {
+    return res.status(406).json({ error: "Not Acceptable" });
   }
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { getAllUsers, deleteUser } from "../controllers/user";
+import { getAllUsers, deleteUser, updateUser } from "../controllers/user";
 import { isAuthenticated, isOwner } from "../middlewares/index";
 
 export default (router: express.Router) => {
   router.get("/users", isAuthenticated, getAllUsers);
   router.delete("/user/:id", isAuthenticated, isOwner, deleteUser);
+  router.put("/user/:id", isAuthenticated, isOwner , updateUser);
 };
