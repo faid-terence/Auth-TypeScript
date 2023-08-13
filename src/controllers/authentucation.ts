556ea15c11e6a07d2b8b2b89dc3abe8187bb9ef6
Faid Terence JABO <faidterence@outlook.com>
2023-08-13 15:26:45 +0200

    feature(User):Login Functionalities

--- src/controllers/authentucation.ts
+++ src/controllers/authentucation.ts
@@ -1,6 +1,5 @@
-
 import express from "express";
-import { getUserByEmail, createUser } from "../db/Users";
+import { getUserByEmail, createUser, UserModel } from "../db/Users";
 import { random, authentication } from "../helpers/index";
 
 export const register = async (req: express.Request, res: express.Response) => {
@@ -16,17 +15,60 @@ export const register = async (req: express.Request, res: express.Response) => {
     }
     const salt = random();
     const user = await createUser({
-        username,
-        email,
-        age,
-        gender,
-        authentication: {
-            salt,
-            password: authentication(salt, password),
-        }
+      username,
+      email,
+      age,
+      gender,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
     });
-    res.status(201).json({message: "User registered Successfully"});
+    return res.status(201).json({ message: "User registered Successfully" });
   } catch (error) {
-    res.status(500).json({error: "Error Occured in Creating User"})
+    return res.status(500).json({ error: "Error Occured in Creating User" });
+  }
+};
+export const loginUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Fill in required fields" });
+    }
+    const existingUser = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+    if (!existingUser) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const expectedHash = authentication(
+      existingUser.authentication.salt,
+      password
+    );
+    if (existingUser.authentication.password !== expectedHash) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const salt = random();
+    existingUser.authentication.sessionToken = authentication(
+      salt,
+      existingUser._id.toString()
+    );
+
+    await existingUser.save();
+    res.cookie("TERENCE-AUTH", existingUser.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+    return res
+      .status(200)
+      .json({
+        message: "Login Successfully",
+        sessionToken: existingUser.authentication.sessionToken,
+      });
+  } catch (error) {
+    return res.status(500).json({ error: "Oops! Server error" });
   }
 };
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 
-import { register } from '../controllers/authentucation';
+import { loginUser, register } from '../controllers/authentucation';
 
 export default (router: express.Router)=> {
     router.post("/auth/register", register);
+    router.post("/auth/login", loginUser);
 }
